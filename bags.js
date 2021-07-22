8658b2c7410d8a27c56b8df54c5f23e08ed249bc
Saarvin-Host <72301169+Saarvin-Host@users.noreply.github.com>
2021-07-22 16:35:35 +0530

    Add files via upload

---
+++ bags.js
@@ -0,0 +1,50 @@
+img="";
+status="";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "STATUS : DETECTING OBJECTS";
+}
+
+function preload(){
+    img = loadImage("bag.jpg");
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        fill(230, 76, 46);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke(255,69,0);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelloaded(){
+    console.log("model loaded!");
+    status = true;
+    objectdetector.detect(img, gotresults);
+}
+
+function gotresults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function back(){
+    window.location = "home_page.html";
+}
---
+++ home_page.js
@@ -0,0 +1,15 @@
+function watches(){
+    window.location = "watches.html";
+}
+
+function bottles(){
+    window.location = "bottles.html";
+}
+
+function bags(){
+    window.location = "bags.html";
+}
+
+function humans(){
+    window.location = "humans.html";
+}
